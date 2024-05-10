e918e12a7054c75875662e61aadc3d8eadcdcf00
Alexandr <mikhalchenkov22@gmail.com>
2024-05-11 01:38:31 +0300

    leaderboard resource

--- resources/leaderboard.py
+++ resources/leaderboard.py
@@ -7,4 +7,16 @@ from data.users import User
 class LeaderboardResource(Resource):
     @staticmethod
     def get():
-        pass
+        session = db_session.create_session()
+        users = session.query(User).filter(User.id < 11).all()
+        result = []
+        for user in users:
+            result.append(
+                    {
+                        "id": user.id,
+                        "name": user.name,
+                        "level_amount": user.level_amount,
+                        "time": user.time,
+                    }
+            )
+        return jsonify(result)
