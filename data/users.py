c509c267c6c113d6d36daf9a6af8abdf3ada7fdf
Alexandr <mikhalchenkov22@gmail.com>
2024-05-11 03:18:16 +0300

    fix

--- data/users.py
+++ data/users.py
@@ -1,13 +1,12 @@
 import sqlalchemy
-from sqlalchemy_serializer import SerializerMixin
 
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase, SerializerMixin):
+class User(SqlAlchemyBase):
     __tablename__ = 'users'
 
-    name = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
+    name = sqlalchemy.Column(sqlalchemy.String, primary_key=True, unique=True, nullable=False)
     password = sqlalchemy.Column(sqlalchemy.String, autoincrement=True)
     level_amount = sqlalchemy.Column(sqlalchemy.Integer)
     time = sqlalchemy.Column(sqlalchemy.Integer)
